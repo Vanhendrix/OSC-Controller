4aa850680d80e4ec360b717b055de00a172375d9
Vanhendrix <28529792+Vanhendrix@users.noreply.github.com>
2025-12-08 17:32:31 +0100

    Adding __init__.py in vendors

---
+++ vendors/__init__.py
@@ -0,0 +1,15 @@
+"""
+    Copyright (C) 2025  Guillaume Hervy
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License version 3 or later.
+
+    See the LICENSE file or <https://www.gnu.org/licenses/> for details.
+"""
+
+"""
+vendors package
+
+Contains third-party dependencies bundled with the OSC Controller add-on, 
+such as the python-osc library (in vendors/pythonosc)
+"""
